6b826d1ee4b194c4a3593fa30d761a37253f8a3c
Fankyzene <marjai.janos@gmail.com>
2022-06-22 17:03:12 +0200

    js wut

---
+++ frontend/script.js
@@ -0,0 +1,21 @@
+console.log('Hello World!');
+
+function loadEvent() {
+    console.log('Loaded');
+
+    let rootElement = document.getElementById('root')
+
+    rootElement.innerHTML = "Sziasztok"
+    /*console.dir(document.getElementById('root'))*/
+    /*console.dir(document.querySelector('#root'))*/
+
+
+
+    rootElement.innerHTML += "Ciao"
+
+    rootElement.insertAdjacentHTML('beforeend', `
+        <h1> This is the title</h1 >`)
+
+}
+
+window.addEventListener('load', loadEvent)
